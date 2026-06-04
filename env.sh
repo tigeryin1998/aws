fae548782528d052f8d7ac4eabc8795b1c32656b
tigeryin1998 <tigeryin1998@hotmail.com>
2026-06-04 15:06:13 +1000

    add env.sh

---
+++ env.sh
@@ -0,0 +1,9 @@
+export AWS_REGION=us-east-1
+export S3_BUCKET_NAME=a2-s3-bucket-672530258965-us-east-1-an
+export FIXED_SIZE_QUEUE_URL=https://sqs.us-east-1.amazonaws.com/672530258965/a2-fixed-size-queue
+export PARAGRAPH_AWARE_QUEUE_URL=https://us-east-1.amazonaws.com/672530258965/a2-paragraph-aware-queue
+export DATABASE_URL='postgresql://postgres:a2databasepwd@a2-postgres.cdppqo0m3twv.us-east-1.rds.amazonaws.com:5432/pdf_rag'
+
+export MAX_FILE_SIZE_MB=5
+export SQS_WAIT_TIME_SECONDS=20
+export SQS_VISIBILITY_TIMEOUT=300
